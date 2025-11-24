4fa86ddf58dfbc863985ff26debb77166fab7c2a
M3ystic <datweneb@sunbird.ece.ncsu.edu>
2025-11-23 21:03:22 -0500

    file commited but trouble running program

--- attack_v2/attacker.c
+++ attack_v2/attacker.c
@@ -1,33 +1,31 @@
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <x86intrin.h>
 
-#include "attacker.h"  // only for SPY_PIPE and ZERO_COUNT
+#include "attacker.h"
 
 int main() {
-
-    // --- 1. Wake up victim (pipe sync) ---
+    // (1) Wake victim via pipe
     uint8_t *zeroes = calloc(ZERO_COUNT, 1);
-    assert(zeroes != NULL);
+    assert(zeroes);
 
-    FILE *pipe = fopen(SPY_PIPE, "wb+");
-    assert(pipe != NULL);
+    FILE *pipe = fopen(SPY_PIPE, "wb");
+    assert(pipe);
 
-    size_t ret = fwrite(zeroes, 1, ZERO_COUNT, pipe);
-    assert(ret == ZERO_COUNT);
+    size_t written = fwrite(zeroes, 1, ZERO_COUNT, pipe);
+    assert(written == ZERO_COUNT);
 
     fclose(pipe);
     free(zeroes);
 
-    // --- 2. MOV-only attacker loop (your side channel hammer) ---
+    // (2) MOV-only attacker loop
     register uint64_t a = 1, b = 2;
 
     for (uint64_t i = 0; i < 2000000000ULL; i++) {
         __asm__ volatile(
-            "mov %1, %0\n\t"
+            "mov %1, %0"
             : "+r"(a)
             : "r"(b)
         );
--- attack_v2/sync.sh
+++ attack_v2/sync.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
-make clean
-make
+make -s
 
-pkill attacker
 pkill ecc
+pkill attacker
 rm -f pipe.fifo
 
-# 1. Create pipe for synchronization
 mkfifo pipe.fifo
 
-# 2. Start ECC victim (blocked on pipe)
-taskset -c 0 ./ecc M 1000 $1 &   # $1 = scalar you pass
+# (1) Start victim blocked on fread()
+taskset -c 0 ./ecc M 1000 $1 &
 
-sleep 0.1   # Let victim reach fread() and block
+sleep 0.05
 
-# 3. Run attacker with perf AND wake the victim (pipe sync)
+# (2) Run perf + attacker (attacker wakes victim *during* its startup)
 taskset -c 24 perf stat -e \
-    move_elimination.int_eliminated,\
-    move_elimination.int_not_eliminated,\
-    cycles,instructions,\
-    uops_issued.any,\
+    move_elimination.int_eliminated \
+    move_elimination.int_not_eliminated \
+    cycles \
+    instructions \
+    uops_issued.any \
     idq_uops_not_delivered.core \
     ./attacker
 
-# 4. Cleanup
 rm -f pipe.fifo
 pkill attacker
 
-#./sync.sh FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
-#./sync_me.sh 000000000000000000000000000000000001
-
+# ./sync.sh FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+# ./sync.sh 00000000000000000000000000000000
